c9e760281226cf808a91815534ac71460c68ed4a
easy00000000 <easy0000@foxmail.com>
2018-04-25 17:54:58 +0800

    .

--- dockerfile/crawl_worker/load_celery.sh
+++ dockerfile/crawl_worker/load_celery.sh
@@ -1,2 +1,2 @@
 cd /home/work/spiders
-celery --loglevel=info -A tasks.scrapy_task worker --max-tasks-per-child 1 -n gw_1@%h
+celery --loglevel=info -A tasks.scrapy_task worker --max-tasks-per-child 1 -n pb_1@%h
